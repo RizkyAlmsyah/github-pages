a11b31c18d47ecc483d49be320cb39023b2bdee4
RizkyAlmsyah <rizkyalamsyah@it.student.pens.ac.id>
2020-07-17 15:55:07 +0700

    Updates

---
+++ precache-manifest.9f7dd9d40f8bea55c0595396c7381f55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2189d5dc4b234316639cf34c493db94",
+    "url": "/github-pages/index.html"
+  },
+  {
+    "revision": "a385b31cfa828b9e26ac",
+    "url": "/github-pages/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b5bf4218c51161a74f4a",
+    "url": "/github-pages/static/css/main.4599dff2.chunk.css"
+  },
+  {
+    "revision": "a385b31cfa828b9e26ac",
+    "url": "/github-pages/static/js/2.728ae378.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/github-pages/static/js/2.728ae378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bf4218c51161a74f4a",
+    "url": "/github-pages/static/js/main.7cc45fe3.chunk.js"
+  },
+  {
+    "revision": "f9fb8de36b6c08020211",
+    "url": "/github-pages/static/js/runtime-main.74b94580.js"
+  }
+]);
